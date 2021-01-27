69e29ae686f5ec524cf7315bec004a6c0cedd269
Phillo93 <philipp.frentzen@gmail.com>
2021-01-27 20:45:28 +0100

    firstversion

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function showInput() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    console.log(nameField)
+    console.log(emailField)
+    console.log(messageField)
+
+}
+
+/*funktioniert nicht ..noch nicht fertig...*/
